21bd442829671891c1c3d820df42d5a91172e51e
Ashish Roy <58362174+RoyAshish80@users.noreply.github.com>
2026-01-13 18:44:24 +0530

    Establishing the connection between frontend and the backend.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get('/', (req:Request , res: Response) => {
   res.send('Hello World from OpenAI Playground!');
 });
 
+app.get('/api/hello', (req:Request , res: Response) => {
+  res.send('Hello World from OpenAI Playground!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
